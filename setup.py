9bb12524265fd7354e292a366cd93c5bdbe2f7ae
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2025-12-17 15:13:25 +0300

    1.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-node-modules',
-    version='1.0.4',
+    version='1.0.5',
     packages=find_packages(),
     include_package_data=True,
     author_email='magilyas.doma.09@list.ru',
